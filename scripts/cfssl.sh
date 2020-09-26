3b08e6ca2f5c86b7ead68c6ce66521c69c4891cf
Aruna Lakmal <arunarandunu@gmail.com>
2020-09-26 22:51:42 +0530

    admin-csr.json

--- scripts/cfssl.sh
+++ scripts/cfssl.sh
@@ -4,3 +4,13 @@ sudo curl -s -L -o /bin/cfssl https://pkg.cfssl.org/R1.2/cfssl_linux-amd64
 sudo curl -s -L -o /bin/cfssljson https://pkg.cfssl.org/R1.2/cfssljson_linux-amd64
 sudo curl -s -L -o /bin/cfssl-certinfo https://pkg.cfssl.org/R1.2/cfssl-certinfo_linux-amd64
 sudo chmod +x /bin/cfssl*
+
+#-------
+cfssl gencert -initca ./ca/ca-csr.json | cfssljson -bare ca
+
+cfssl gencert \
+  -ca=ca.pem \
+  -ca-key=ca-key.pem \
+  -config=./ca/ca-config.json \
+  -profile=kubernetes \
+  ./ca/admin-csr.json | cfssljson -bare admin
